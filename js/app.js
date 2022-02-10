048b94b4c47124669c8a80e1202bc5f3da1097aa
gkatsikis <geo.bjj.kat@gmail.com>
2022-02-10 18:50:48 -0500

    html boilerplate and adding images

---
+++ js/app.js
@@ -0,0 +1,40 @@
+// ---------------------VARIABLES------------------------
+
+
+
+
+
+
+
+// ----------------EVENT LISTENERS AND DOM STUFF----------
+
+
+
+
+
+
+// -------------------FUNCTIONS---------------------------
+
+function init() {
+
+}
+
+function render() {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+function webcamEnding() {
+
+}
