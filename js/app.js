3fc667eed8d31622b735fa1a26deaac82bafc8a3
gkatsikis <geo.bjj.kat@gmail.com>
2022-02-11 10:16:00 -0500

    adding const variables and event listeners

--- js/app.js
+++ js/app.js
@@ -9,8 +9,15 @@
 // ----------------EVENT LISTENERS AND DOM STUFF----------
 
 
+const GA = document.querySelector('#GA-building')
 
+const HH = document.querySelector('#haunted-house')
 
+const player = document.querySelector('#player-figure')
+
+player.addEventListener('click', () => {
+  console.log('player')
+})
 
 
 // -------------------FUNCTIONS---------------------------
