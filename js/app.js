fae4ef617d9167292ccb3f2c768af97e42ac3fa8
gkatsikis <geo.bjj.kat@gmail.com>
2022-02-11 14:03:53 -0500

    adding objects

--- js/app.js
+++ js/app.js
@@ -1,48 +1,56 @@
 // ---------------------VARIABLES------------------------
 const images = [
   'GA-building1.png', 'haunted-house.png', 'player.png', 
-  'road.jpg', 'classroom.jpg', 'computer-lab.jpg','GA-inside.png','GA-room1.jpg','inside-house.jpg']
+  'road.jpg', 'classroom.jpg', 'computer-lab.jpg','GA-inside.png','GA-room1.jpg','inside-house.jpg', 'portal.png']
 
 const start = {
   background: images[3],
   option1: images[1],
   option2: images[0],
+  message: "Make your choice and stop bothering me",
 }
 
 // changeBackground(8)
 
-// const hauntedHouse = {
-//   background: images[8],
-//   option1: images[],
-//   option2: images[]
-// }
+const hauntedHouse = {
+  background: images[8],
+  option1: images[9],
+  option2: images[9],
+  message: "I probably wouldn't choose either of them if i were you",
+}
 
-// const GABuilding = {
-//   background: images[5],
-//   option1: images[],
-//   option2: images[]
-// }
-
-
-
-
-const scenario = [0,1,2,3,4,5,6,7,8]
+const GABuilding = {
+  background: images[5],
+  option1: images[9],
+  option2: images[9],
+  message: "Maybe...you should have chosen Flatiron?"
+}
 
 
-// ----------------EVENT LISTENERS AND DOM STUFF----------
+
+function changeBackground(obj) {
+  document.body.style.backgroundImage = `url("../assets/images/${obj.background}")`
+}
 
 
-const GA = document.querySelector('#GA-building')
 
-const HH = document.querySelector('#haunted-house')
+//---------------------CACHED ELEMENT REFERENCES-----
+
 
 const player = document.querySelector('#player-figure')
 
-GA.addEventListener('click', () => {
+const option1 = document.querySelector("#option1")
+
+const option2 = document.querySelector("#option2")
+
+
+// ----------------EVENT LISTENERS---------------
+
+option1.addEventListener('click', () => {
   
 })
 
-HH.addEventListener('click', () => {
+option2.addEventListener('click', () => {
   
 })
 
@@ -56,33 +64,32 @@ player.addEventListener('click', () => {
 // -------------------FUNCTIONS---------------------------
 
 function init() {
-  changeBackground(3)
-  changeOption1()
-  changeOption2()
-  changeMessage()
+  
 }
 
 function render() {
 
 }
 
-a()
-// changeOption1('#haunted-house', images[5])
-function a (evt) {
-  let img = document.createElement('img')
-  img.src = `../assets/images/computer-lab.jpg`
-  img.id = "hh"
+function replace (obj) {
+  changeBackground(obj) // done
+  changeOption1(obj)
+  changeOption2(obj)
+  changeMessage(obj)
 }
 
-function changeBackground(evt) {
-  document.body.style.backgroundImage = `url("../assets/images/${images[evt]}")`
+
+// changeOption1('#haunted-house', images[5])
+function changeImageSrc (evt, newSrc) {
+  evt.src=`../assets/images/${newSrc}`
 }
+
+
 
 
 function changeOption1(remove, add) {
   let image1 = document.querySelector(remove)
   image1.parentNode.removeChild(image1)
-  a(add)
 }
 
 function changeOption2(evt) {
