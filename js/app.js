74fc7559923ff55055821b356212914341f9e5f7
gkatsikis <geo.bjj.kat@gmail.com>
2022-02-13 12:42:23 -0500

    adding content to objects

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ let lvlId = null
 
 const images = [
   'GA-building1.png', 'haunted-house.png', 'player.png', 
-  'road.jpg', 'classroom.jpg', 'computer-lab.jpg','GA-inside.png','GA-room1.jpg','inside-house.jpg', 'portal.png']
+  'road.jpg', 'classroom.jpg', 'computer-lab.jpg','GA-inside.png','GA-room1.jpg','inside-house.jpg', 'portal.png', 'basement.jpg', 'final.png']
 
 
 const start = {
@@ -12,22 +12,25 @@ const start = {
   option2: images[0],
   message: "You wake up on a dark road with two choices before you...do you enter the house that screams of malevolence...or do you enter the building that offers knowledge that leads one to madness?",
   level: 0,
+  audio: 'ambience.mp3',
 }
 
 const hauntedHouse = {
   background: images[8],
   option1: images[9],
   option2: images[9],
-  message: "I probably wouldn't choose either of them if i were you",
+  message: "Upon entering, you are confronted with disturbing figures and hear footsteps approaching. Do you head to the basement or run upstairs?",
   level: 1,
+  audio: 'demongirl.mp3',
 }
 
 const GABuilding = {
   background: images[5],
   option1: images[9],
   option2: images[9],
-  message: "Maybe...you should have chosen Flatiron?",
+  message: "Maybe...you should have chosen Flatiron?/n ",
   level: 2,
+  audio: 'creaking-door.mp3'
 }
 
 const classroom = {
@@ -36,47 +39,53 @@ const classroom = {
   option2: images[9],
   message: "Can't believe it actually worked",
   level: 5,
+  // audio: //chanting,
 }
 
 const GAInside = {
   background: images[6],
   option1: images[9],
   option2: images[9],
-  message: "Is it working????",
+  message: "Ah, I see you've come to join us...welcome to level 6",
   level: 6,
+  audio: 'heartbeat.mp3',
 }
 
-// const basement = {
-//   background:
-//   option1:
-//   option2:
-//   message:
-//   level: 3
+const basement = {
+  background: images[10],
+  option1:images[9],
+  option2: images[9],
+  message: "Why would you go into the basement of a house like this? This is where we like to play...",
+  level: 3,
+  audio: 'demongirl2.mp3',
+}
+
+const upstairs = {
+  background: images[11],
+  option1: images[9],
+  option2: images[9],
+  message: "You come upstairs and there's someone on the computer...do you try to climb out through the window or approach them?",
+  level: 4,
+  audio: 'ambience2.mp3',
+}
+
+// const escapeRoom = {
+//   background: body on the street
+//   option1: "",
+//   option2: "",
+//   message: "I'm afraid you had a little fall... You survived to level 7",
+//   level: 7,
+//   audio: 'campy-laugh.mp3',
 // }
 
-// const upstairs = {
-//   background:
-//   option1:
-//   option2:
-//   message:
-//   level: 4
-// }
-
-// const escape = {
-//   background:
-//   option1:
-//   option2:
-//   message:
-//   level: 7
-// }
-
-// const final = {
-//   background:
-//   option1:
-//   option2:
-//   message:
-//   level: 8
-// }
+const final = {
+  background: images[11],
+  option1: "",
+  option2: "",
+  message: "Congratulations...you made it to the final level. Look behind you.",
+  level: 8,
+  audio: 'hell-ambience.mp3',
+}
 
 
 //---------------------CACHED ELEMENT REFERENCES-----
@@ -99,6 +108,8 @@ option1.addEventListener('click', () => {
     replace(basement)
   } else if (lvlId === 0) {
     replace(hauntedHouse)
+  } else if (lvlId === 4) {
+    replace(escapeRoom)
   }
 })
 
@@ -109,7 +120,9 @@ option2.addEventListener('click', () => {
     replace(upstairs)
   } else if (lvlId === 0) {
     replace(GABuilding)
-  }
+  // } else if (lvlId === 4) {
+  //   webcam.start()
+  // }
 })
 
 player.addEventListener('click', () => {
@@ -132,6 +145,7 @@ function replace(obj) {
   changeOption1(obj) // done
   changeOption2(obj) // done
   changeMessage(obj) // done
+  changeAudio(obj)
 }
 
 function changeBackground(obj) {
@@ -154,6 +168,16 @@ function changeLevel(obj) {
   lvlId = obj.level
   return lvlId
 }
+
+function changeAudio(obj) {
+    let audioFile = new Audio(`../assets/audio/${obj.audio}`)
+    audioFile.volume = .25
+    audioFile.play()
+    audioFile.addEventListener('ended', () => {
+      audioFile.currentTime = 0
+      audioFile.play()
+    }, false)
+    }
 
 // function webcamEnding() {
 
